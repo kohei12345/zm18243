8bf1bb08516f861f2d64ad633191d7a69fa53d75
kohei12345 <zm18243@josai.ac.jp>
2020-10-20 02:06:34 +0000

    1022

--- ex6-1.js
+++ ex6-1.js
@@ -4,13 +4,3 @@ alert ('hello function!');
 function saygoodbye(){
 alert ('goodbye function!');
 }
-var who = 'world';
-function sayehello(){
-alert('hello,'+ who + '!');
-}
-function taro(){
- who = '太郎';
-}
-function hanako(){
- who = '花子';
-}
